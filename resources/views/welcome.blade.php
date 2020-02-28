a0a136056fc0e471ffe5d4f6f7dec094f53fffd4
firasmpratama <firasmpratama17@gmail.com>
2020-02-28 19:52:44 +0700

    change title website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Oneder &mdash; Website Template by Colorlib</title>
+    <title>Widuri Computer Community &mdash; Website by WCC</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
